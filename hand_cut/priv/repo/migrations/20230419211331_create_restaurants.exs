d81c00c6d1168157e476b15246bd9308ee4dbac7
Mohamed Aly <mksaga@hey.com>
2023-04-20 17:18:25 -0400

    Adds migrations

---
+++ hand_cut/priv/repo/migrations/20230419211331_create_restaurants.exs
@@ -0,0 +1,21 @@
+defmodule HandCut.Repo.Migrations.CreateRestaurants do
+  use Ecto.Migration
+
+  def change do
+    create table(:restaurants) do
+      add :name, :string
+      add :code, :string
+      add :active, :boolean
+      add :activated_at, :utc_datetime
+      add :address, :string
+      add :phone, :string
+      # Ecto Enum in the schema
+      add :area, :string
+      # Ecto Enum in the schema
+      add :cuisine, :string
+      add :url, :string
+      add :instagram, :string
+      add :google_maps, :string
+    end
+  end
+end
