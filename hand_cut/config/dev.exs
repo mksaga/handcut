9947734792b5d3afc03df51080b601bf14c3f950
Mohamed Aly <mksaga@hey.com>
2023-04-26 21:19:37 -0400

    Minor config tweaks

--- hand_cut/config/dev.exs
+++ hand_cut/config/dev.exs
@@ -2,9 +2,9 @@ import Config
 
  config :hand_cut, HandCut.EventStore,
    serializer: Commanded.Serialization.JsonSerializer,
+   database: "eventstore_dev",
    username: "postgres",
    password: "postgres",
-   database: "eventstore_dev",
    hostname: "localhost",
    pool_size: 10
 
@@ -12,4 +12,5 @@ config :hand_cut, HandCut.Projections.Repo,
   database: "hand_cut_projections",
   username: "postgres",
   password: "postgres",
-  hostname: "localhost"
+  hostname: "localhost",
+  pool_size: 10
