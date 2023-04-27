40b7d037b56298081737740045e6d7f60ce70f1a
Mohamed Aly <mksaga@hey.com>
2023-04-26 21:23:26 -0400

    Cleans up projection

--- hand_cut/lib/impl/projections/restaurant.ex
+++ hand_cut/lib/impl/projections/restaurant.ex
@@ -1,20 +1,40 @@
 defmodule HandCut.Restaurant do
   use Ecto.Schema
+  import Ecto.Changeset
 
   alias HandCut.Restaurant.Areas
   alias HandCut.Restaurant.Cuisines
 
   schema "restaurants" do
-    field :name, :string
-    field :code, :string
-    field :active, :boolean
-    field :activated_at, :utc_datetime
-    field :address, :string
-    field :phone, :string
-    field :area, Ecto.Enum, values: Areas.all_areas()
-    field :cuisine, Ecto.Enum, values: Cuisines.all_cuisines()
-    field :url, :string
-    field :instagram, :string
-    field :google_maps, :string
+    field(:name, :string)
+    field(:code, :string)
+    field(:active, :boolean)
+    field(:cash_only, :boolean)
+    field(:activated_at, :utc_datetime)
+    field(:address, :string)
+    field(:phone, :string)
+    field(:area, Ecto.Enum, values: Areas.all_areas())
+    field(:cuisine, Ecto.Enum, values: Cuisines.all_cuisines())
+    field(:url, :string)
+    field(:instagram, :string)
+    field(:google_maps, :string)
+  end
+
+  def changeset(restaurant, attrs) do
+    restaurant
+    |> cast(attrs, [
+      :name,
+      :code,
+      :active,
+      :cash_only,
+      :activated_at,
+      :address,
+      :phone,
+      :area,
+      :cuisine,
+      :url,
+      :instagram,
+      :google_maps
+    ])
   end
 end
