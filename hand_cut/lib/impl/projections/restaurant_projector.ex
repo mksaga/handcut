7701f76c771a0724e0bebd02cccb3f60221b93e3
Mohamed Aly <mksaga@hey.com>
2023-04-26 21:23:15 -0400

    Updates projector to use changeset

--- hand_cut/lib/impl/projections/restaurant_projector.ex
+++ hand_cut/lib/impl/projections/restaurant_projector.ex
@@ -1,38 +1,47 @@
- defmodule HandCut.Projectors.RestaurantProjector do
-
+defmodule HandCut.Projectors.RestaurantProjector do
   alias HandCut.Restaurant
 
-   use Commanded.Projections.Ecto,
-     application: HandCut.Runtime.App,
-     repo: HandCut.Projections.Repo,
-     name: "restaurant"
+  use Commanded.Projections.Ecto,
+    application: HandCut.Runtime.App,
+    repo: HandCut.Projections.Repo,
+    name: "restaurant"
 
-   # project %HandCut.Events.RestaurantCreated{
-   #   name: name,
-   #   id: code,
-   #   address: address,
-   #   phone: phone,
-   #   area: area,
-   #   cuisine: cuisine,
-   #   url: url,
-   #   instagram: instagram,
-   #   google_maps: google_maps,
-   # }, _metadata, fn multi ->
-   #   Ecto.Multi.insert(
-   #     multi,
-   #     :restaurant,
-   #     %Restaurant{
-   #       name: name,
-   #       code: code,
-   #       active: false,
-   #       activated_at: nil,
-   #       address: address,
-   #       phone: phone,
-   #       area: area,
-   #       cuisine: cuisine,
-   #       url: url,
-   #       instagram: instagram,
-   #       google_maps: google_maps,
-   #     })
-   # end
- end
+  project(
+    %HandCut.Events.RestaurantCreated{
+      name: name,
+      id: code,
+      address: address,
+      cash_only: cash_only,
+      phone: phone,
+      area: area,
+      cuisine: cuisine,
+      url: url,
+      instagram: instagram,
+      google_maps: google_maps
+    },
+    _metadata,
+    fn multi ->
+      Ecto.Multi.insert(
+        multi,
+        :restaurant,
+        Restaurant.changeset(
+          %Restaurant{},
+          %{
+            name: name,
+            code: code,
+            active: false,
+            activated_at: nil,
+            address: address,
+            cash_only: cash_only,
+            phone: phone,
+            area: area,
+            cuisine: cuisine,
+            url: url,
+            instagram: instagram,
+            google_maps: google_maps
+          }
+        )
+      )
+    end
+  )
+end
