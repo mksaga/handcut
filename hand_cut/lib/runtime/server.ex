ed5ff098600e70f85fcf698783fe4fbf0cab87c5
Mohamed Aly <mksaga@hey.com>
2023-04-26 21:23:06 -0400

    Updates start children

--- hand_cut/lib/runtime/server.ex
+++ hand_cut/lib/runtime/server.ex
@@ -2,6 +2,8 @@ defmodule HandCut.Runtime.Server do
   def start_link do
     children = [
       HandCut.Runtime.App,
+      HandCut.Projections.Repo,
+      HandCut.Projectors.RestaurantProjector,
     ]
 
     opts = [strategy: :one_for_one, name: HandCut.Supervisor]
