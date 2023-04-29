86623d3569d84c819dd36add3ac3a2b20d6b4855
Mohamed Aly <mksaga@hey.com>
2023-04-29 17:58:35 -0500

    Adds decoding logic for datetime

--- hand_cut/lib/impl/events/restaurant_activated.ex
+++ hand_cut/lib/impl/events/restaurant_activated.ex
@@ -6,3 +6,14 @@ defmodule HandCut.Events.RestaurantActivated do
     :activated_at,
   ]
 end
+
+defimpl Commanded.Serialization.JsonDecoder, for: HandCut.Events.RestaurantActivated do
+  @doc """
+  Parse the datetime included
+  """
+  def decode(%HandCut.Events.RestaurantActivated{activated_at: datetime} = event) do
+    {:ok, dt, _} = DateTime.from_iso8601(datetime)
+    truncated = DateTime.truncate(dt, :second)
+    %HandCut.Events.RestaurantActivated{event | activated_at: truncated}
+  end
+end
