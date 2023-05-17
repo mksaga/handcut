be917ab512fb40593e21b242a4fc83775417d118
Mohamed Aly <mksaga@hey.com>
2023-05-17 03:51:56 +0300

    Removes options from view

--- hand_cut_web/lib/hand_cut_web_web/views/search_view.ex
+++ hand_cut_web/lib/hand_cut_web_web/views/search_view.ex
@@ -2,14 +2,4 @@ defmodule HandCutWebWeb.SearchView do
   alias HandCutWebWeb.SearchUI
 
   use HandCutWebWeb, :view
-
-    def options() do
-    [
-      %{
-        :value => "nj_jersey_city",
-        :label => "(NJ) Jersey City"
-      }
-    ]
-  end
-
 end
