068a598c0cc722b5f8ec37be5a7b3d5d4298ebb0
Mohamed Aly <mksaga@hey.com>
2023-05-17 03:48:38 +0300

    Adds /search route, cleans up root.html.heex

--- hand_cut_web/lib/hand_cut_web_web/router.ex
+++ hand_cut_web/lib/hand_cut_web_web/router.ex
@@ -18,6 +18,7 @@ defmodule HandCutWebWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :index
+    get "/search", SearchController, :search
   end
 
   # Other scopes may use custom stacks.
