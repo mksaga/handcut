206266149bb44cca8f9c410da1faed31a681c890
Mohamed Aly <mksaga@hey.com>
2023-05-18 22:16:17 +0300

    Adds /restaurant router entry, removes unused ctrl

--- hand_cut_web/lib/hand_cut_web_web/controllers/restaurant_controller.ex
+++
@@ -1,9 +0,0 @@
-defmodule HandCutWebWeb.RestaurantController do
-  use HandCutWebWeb, :controller
-  alias HandCut.Restaurant.{Cuisines, Areas}
-
-  def show(conn, params) do
-    IO.inspect params["code"]
-    render(conn, "restaurant.html")
-  end
-end
--- hand_cut_web/lib/hand_cut_web_web/router.ex
+++ hand_cut_web/lib/hand_cut_web_web/router.ex
@@ -18,9 +18,9 @@ defmodule HandCutWebWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :index
+    get "/restaurant/:code", RestaurantController, :show
     live "/search", SearchLive, :index
     live "/results", ResultsLive, :index
-    get "/restaurant/:code", RestaurantController, :show
   end
 
   # Other scopes may use custom stacks.
