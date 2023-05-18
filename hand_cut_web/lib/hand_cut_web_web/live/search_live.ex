73ac5ff0a805ea34bdfd5b87230a60ec29e614b8
Mohamed Aly <mksaga@hey.com>
2023-05-18 04:21:26 +0300

    Clean up SearchLive view

--- hand_cut_web/lib/hand_cut_web_web/live/search_live.ex
+++ hand_cut_web/lib/hand_cut_web_web/live/search_live.ex
@@ -16,32 +16,47 @@ defmodule HandCutWebWeb.SearchLive do
 
   def mount(_params, %{}, socket) do
     {:ok,
-     assign(socket, :areas, [])
-     |> assign(:cuisines, [])}
+     socket
+     |> assign(form: to_form(%{"area" => nil, "cuisines" => []}))}
   end
+
+  def handle_event("search", params, socket) do
+    # `params` has what we need!!
+    {:noreply, socket}
+  end
+
+  def handle_event("update", %{"area" => new_area, "cuisines" => new_cuisines} = revised_form, socket) do
+    {:noreply, assign(socket, form: %{revised_form | "area" => new_area, "cuisines" => new_cuisines})}
+end
 
   def render(assigns) do
     ~H"""
     <div class="level">
         <div class="level-item">
-            <h1 class="title">Welcome</h1>
+            <h1 class="title">Welcome to HandCut!</h1>
         </div>
         <div class="level-item">
             <h2 class="subtitle">Let us know where you're looking and what you're craving.</h2>
         </div>
 
+        <.form for={@form} let={f} phx-change="update" phx-submit="search">
+            <div class="level-item">
+                <.live_component module={AreaSelect} id="area-select" options={areas()} form={f}/>
+            </div>
+            <div class="level-item">
+                <.live_component module={CuisineSelect} id="cuisine-select" label="Cuisine(s)" options={cuisines()} form={f}/>
+            </div>
 
+            <div class="level-item">
+                <div class="field">
+                    <div class="control">
+                        <%= submit "Search", class: "button is-link" %>
+                    </div>
+                </div>
+            </div>
+        </.form>
 
-        <div class="level-item">
-            <.live_component module={AreaSelect} id="area-select" options={areas()} />
         </div>
-        <div class="level-item">
-            <.live_component module={CuisineSelect} id="cuisine-select" options={cuisines()} label="Cuisine(s)" />
-        </div>
-        <div class="level-item">
-            <SearchUI.submit />
-        </div>
-    </div>
     """
   end
 end
