07a02acfb0766ae99955a117019b0cbd4a3763f8
Mohamed Aly <mksaga@hey.com>
2023-05-18 16:41:18 +0300

    Adds Results live view

--- hand_cut_web/lib/hand_cut_web_web/live/results_live.ex
+++ hand_cut_web/lib/hand_cut_web_web/live/results_live.ex
@@ -1,18 +1,33 @@
 defmodule HandCutWebWeb.ResultsLive do
   use HandCutWebWeb, :live_view
+  alias HandCut.Projections.Restaurant
+  alias HandCutWebWeb.RestaurantResult
 
   def mount(params, %{}, socket) do
+    results = Restaurant.filter_search(params)
+
     {:ok,
      socket
      |> assign(area: params["area"])
-     |> assign(cuisines: params["cuisines"])}
+     |> assign(cuisines: params["cuisines"])
+     |> assign(results: results)}
   end
 
   def render(assigns) do
     ~H"""
-    <div>Results</div>
-    <div><%= @area %></div>
-    <div><%= @cuisines %></div>
+    <div class="level">
+        <div class="level-item">
+            <h1 class="title">Results</h1>
+        </div>
+        <div class="level-item">
+            <h2 class="subtitle"><%= length(@results) %> <%= @cuisines %> in <%= @area %></h2>
+        </div>
+    </div>
+    <div>
+        <%= for result <- @results do %>
+            <.live_component module={RestaurantResult} id={"result-#{result.code}"} result={result}} />
+        <% end %>
+    </div>
     """
   end
 end
