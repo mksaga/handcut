70804dca61bf8c66d277f4cf59d318eeb85e26b3
Mohamed Aly <mksaga@hey.com>
2023-05-19 02:58:05 +0300

    Deletes extra controller

--- hand_cut_web/lib/hand_cut_web_web/controllers/search_controller.ex
+++
@@ -1,9 +0,0 @@
-defmodule HandCutWebWeb.RestaurantController do
-  use HandCutWebWeb, :controller
-  alias HandCut.Projections.Restaurant
-
-  def show(conn, params) do
-    restaurant = Restaurant.get_code("restaurant_" <> params["code"])
-    render(conn, "restaurant.html", restaurant: restaurant)
-  end
-end
