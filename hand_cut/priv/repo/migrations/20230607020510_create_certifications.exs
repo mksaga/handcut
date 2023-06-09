c6cda52f16d23674c587055a3fea1ec0a68936e5
Mohamed Aly <mksaga@hey.com>
2023-06-09 18:32:30 -0400

    Adds migration for certifications

---
+++ hand_cut/priv/repo/migrations/20230607020510_create_certifications.exs
@@ -0,0 +1,21 @@
+defmodule HandCut.Projections.Repo.Migrations.CreateCertifications do
+  use Ecto.Migration
+
+  def change do
+    create table(:certifications) do
+      add :restaurant_id, :string
+      add :code, :string
+      add :retailer_id, :string
+      add :supplier_id, :string
+      # Ecto Enum in the schema
+      add :type, :string
+      # Ecto Enum in the schema
+      add :products, {:array, :string}
+      add :expiration, :date
+      add :issuing_agency, :string
+
+      timestamps()
+    end
+
+  end
+end
