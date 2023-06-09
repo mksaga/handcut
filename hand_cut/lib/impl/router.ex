c734b843eabf74ac9c6a88d3d0b00f672e2ca11c
Mohamed Aly <mksaga@hey.com>
2023-06-09 18:22:03 -0400

    Adds dispatch for CreateCertification

--- hand_cut/lib/impl/router.ex
+++ hand_cut/lib/impl/router.ex
@@ -1,13 +1,19 @@
 defmodule HandCut.Router do
   use Commanded.Commands.Router
 
-  alias HandCut.Aggregates.{ActivationRequest, Restaurant}
+  alias HandCut.Aggregates.{ActivationRequest, Certification, Restaurant}
   alias HandCut.Commands.{ActivateRestaurant, CreateRestaurant}
+  alias HandCut.Commands.{CreateCertification}
   alias HandCut.Commands.{ApproveActivationRequest, CreateActivationRequest}
+  # alias HandCut.Commands.Middleware.ValidateCommand
+
+  # middleware ValidateCommand
 
   identify(Restaurant, by: :id)
+  identify(Certification, by: :id)
   identify(ActivationRequest, by: :restaurant_code)
 
   dispatch [ActivateRestaurant, CreateRestaurant], to: Restaurant
   dispatch [ApproveActivationRequest, CreateActivationRequest], to: ActivationRequest
+  dispatch CreateCertification, to: Certification
 end
