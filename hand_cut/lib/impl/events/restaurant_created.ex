bf471312430fe0036dab3d2e5b5f6e25e6a28d7f
Mohamed Aly <mksaga@hey.com>
2023-06-10 14:19:35 -0400

    Adds lat/long to created event

--- hand_cut/lib/impl/events/restaurant_created.ex
+++ hand_cut/lib/impl/events/restaurant_created.ex
@@ -10,36 +10,9 @@ defmodule HandCut.Events.RestaurantCreated do
     :area,
     :cuisine,
     :url,
+    :latitude,
+    :longitude,
     :instagram,
     :google_maps,
   ]
-
-  defimpl Commanded.Event.Upcaster, for: RestaurantCreated do
-    def upcast(%RestaurantCreated{} = event, _metadata) do
-      %RestaurantCreated{
-        id: id,
-        name: name,
-        address: address,
-        phone: phone,
-        area: area,
-        cuisine: cuisine,
-        url: url,
-        instagram: instagram,
-        google_maps: google_maps
-        } = event
-
-      %HandCut.Events.RestaurantCreated{
-        id: id,
-        name: name,
-        address: address,
-        phone: phone,
-        cash_only: false,
-        area: area,
-        cuisine: cuisine,
-        url: url,
-        instagram: instagram,
-        google_maps: google_maps
-        }
-    end
-  end
 end
