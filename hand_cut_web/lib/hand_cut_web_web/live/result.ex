f5d7a129e0b66a5e47b39a98e1e468d469a61bd0
Mohamed Aly <mksaga@hey.com>
2023-06-10 12:41:42 -0400

    Clean up result layout

--- hand_cut_web/lib/hand_cut_web_web/live/result.ex
+++ hand_cut_web/lib/hand_cut_web_web/live/result.ex
@@ -3,23 +3,27 @@ defmodule HandCutWebWeb.RestaurantResult do
 
   alias HandCutWebWeb.Components
 
+  defp humanize_certification_type("certified_hand_slaughtered") do
+    "Certified Hand Slaughtered"
+  end
+
   def render(assigns) do
     ~H"""
-    <a href={Routes.restaurant_path(@socket, :show, String.split(@result.code, "_") |> Enum.at(1))}>
-    <div class="card m-2">
-        <div class="card-image">
-            <figure class="image is-4by3">
-                <img src="http://localhost:4000/images/placeholders/1280x960.png" alt="Placeholder image">
-            </figure>
-        </div>
-        <div class="card-content">
+    <a href={Routes.restaurant_path(@socket, :show, String.split(@result.restaurant.code, "_") |> Enum.at(1))}>
+    <div class="card mb-1">
+        <div class="card-content p-4">
             <div class="level is-mobile">
                 <div class="level-left">
-                    <p class="title is-4"><%= @result.name %></p>
+                    <div>
+                      <p class="title is-4 mb-1"><%= @result.restaurant.name %></p>
+                        <%= if @result.certification != nil do %>
+                          <Components.certification_label certification={@result.certification} />
+                        <% end %>
+                    </div>
                 </div>
                 <div class="level-right">
                   <div class="level-item">
-                    <Components.cuisine_label cuisine={@result.cuisine} />
+                    <Components.cuisine_label cuisine={@result.restaurant.cuisine} />
                     </div>
                 </div>
             </div>
