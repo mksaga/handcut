d21958e1879a1c4e860dfa57f0a277fa329202f4
Mohamed Aly <mksaga@hey.com>
2023-06-12 09:23:45 -0400

    Adds cert select component to search

--- hand_cut_web/lib/hand_cut_web_web/live/search_live.ex
+++ hand_cut_web/lib/hand_cut_web_web/live/search_live.ex
@@ -1,11 +1,22 @@
 defmodule HandCutWebWeb.SearchLive do
   use HandCutWebWeb, :live_view
 
-  alias HandCutWebWeb.{AreaSelect, CuisineSelect}
+  alias HandCutWebWeb.{AreaSelect, CertificationSelect}
   alias HandCut.Restaurant.{Cuisines, Areas}
 
   def cuisines() do
     Cuisines.all_cuisines()
+  end
+
+  def certification_types() do
+   # TODO pull this from projection
+   # types = Enum.map(HandCut.Projections.Certification.certification_types, &(elem(&1, 1)))
+   # [{"All", :all} | types]
+   [
+     {"All", :all},
+     {"Hand Slaughtered", :certified_hand_slaughtered},
+     {"Machine Slaughtered", :certified_machine_slaughtered},
+   ]
   end
 
   def areas() do
@@ -15,7 +26,7 @@ defmodule HandCutWebWeb.SearchLive do
   def mount(_params, %{}, socket) do
     {:ok,
      socket
-     |> assign(form: to_form(%{"area" => nil, "cuisines" => []}))}
+     |> assign(form: to_form(%{"area" => nil, "certification_type" => nil}))}
   end
 
   def handle_event("search", params, socket) do
@@ -24,13 +35,17 @@ defmodule HandCutWebWeb.SearchLive do
     {:noreply, push_navigate(socket, to: "/results?#{query}")}
   end
 
-  def handle_event("update", %{"area" => new_area}, socket) do
-    {:noreply, assign(socket, form: %{"area" => new_area})}
+  def handle_event("update", %{"area" => new_area} = form, socket) do
+    {:noreply, assign(socket, form: %{form | "area" => new_area})}
   end
 
-  def handle_event("update", %{"area" => new_area, "cuisines" => new_cuisines} = revised_form, socket) do
-    {:noreply, assign(socket, form: %{revised_form | "area" => new_area, "cuisines" => new_cuisines})}
-end
+  def handle_event("update", %{"certification_type" => certification_type} = form, socket) do
+    {:noreply, assign(socket, form: %{form | "certification_type" => certification_type})}
+  end
+
+  #   def handle_event("update", %{"area" => new_area, "cuisines" => new_cuisines} = revised_form, socket) do
+  #     {:noreply, assign(socket, form: %{revised_form | "area" => new_area, "cuisines" => new_cuisines})}
+  # end
 
   # 18 May 2023: thought about it and since we don't have many zabiha options just yet,
   # will remove cuisine selection from search and show all restaurants in area X
@@ -51,6 +66,10 @@ end
         <.form for={@form} let={f} phx-change="update" phx-submit="search">
             <div class="level-item">
                 <.live_component module={AreaSelect} id="area-select" options={areas()} form={f}/>
+            </div>
+
+            <div class="level-item">
+                <.live_component module={CertificationSelect} id="area-select" options={certification_types()} form={f}/>
             </div>
 
             <div class="level-item">
