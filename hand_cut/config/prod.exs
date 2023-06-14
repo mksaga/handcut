35828bc066fa2fd1cc4076e0decffeb18bae4d4f
Mohamed Aly <mksaga@hey.com>
2023-06-13 23:05:59 -0400

    Creates prod config file

---
+++ hand_cut/config/prod.exs
@@ -0,0 +1,8 @@
+import Config
+
+config :hand_cut, HandCut.Projections.Repo,
+    username: "postgres",
+    password: "g@ZqD/:R&~>6c_`g",
+    database: "projections",
+    hostname: "localhost",
+    pool_size: 10
