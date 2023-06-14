b9ea3e70a485067510cc9bc79a410674dd4320bd
Mohamed Aly <mksaga@hey.com>
2023-06-13 23:11:05 -0400

    Adds eventstore prod config

--- hand_cut/config/prod.exs
+++ hand_cut/config/prod.exs
@@ -1,5 +1,13 @@
 import Config
 
+ config :hand_cut, HandCut.EventStore,
+   serializer: Commanded.Serialization.JsonSerializer,
+   database: "eventstore",
+   username: "postgres",
+    password: "g@ZqD/:R&~>6c_`g",
+   hostname: "localhost",
+   pool_size: 10
+
 config :hand_cut, HandCut.Projections.Repo,
     username: "postgres",
     password: "g@ZqD/:R&~>6c_`g",
