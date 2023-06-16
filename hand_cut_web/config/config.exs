e6984a949a9f37965f402772aa85bd9ae61e190b
Mohamed Aly <mksaga@hey.com>
2023-06-15 21:25:19 -0400

    Cut trailing comma

--- hand_cut_web/config/config.exs
+++ hand_cut_web/config/config.exs
@@ -5,4 +5,4 @@ config :dart_sass, :version, "1.61.0"
 
 
 config :hand_cut_web, HandCutWebWeb.Endpoint,
-  live_view: [signing_salt: Application.get_env(:hand_cut_web, :live_view_signing_salt)],
+  live_view: [signing_salt: Application.get_env(:hand_cut_web, :live_view_signing_salt)]
