bb93a8346a42ef8ab2cff939a7f859d3b9aab425
Mohamed Aly <mksaga@hey.com>
2023-06-15 21:26:52 -0400

    Adds line to load signing salt in config

--- hand_cut_web/config/config.exs
+++ hand_cut_web/config/config.exs
@@ -3,6 +3,8 @@ import Config
 config :phoenix, :json_library, Jason
 config :dart_sass, :version, "1.61.0"
 
+config :hand_cut_web, :live_view_signing_salt, System.fetch_env!("LIVE_VIEW_SIGNING_SALT")
+
 
 config :hand_cut_web, HandCutWebWeb.Endpoint,
   live_view: [signing_salt: Application.get_env(:hand_cut_web, :live_view_signing_salt)]
