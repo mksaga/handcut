e3395c909140faead978d1d13bc204ef99eff327
Mohamed Aly <mksaga@hey.com>
2023-06-17 00:21:26 -0400

    Starts on deploy

--- deploy.sh
+++ deploy.sh
@@ -14,3 +14,14 @@ mix deps.get --only prod
 # Optional CI steps
 # mix test
 # mix credo --strict
+
+# Set mix_env so subsequent mix steps don't need to specify it
+export MIX_ENV=prod
+
+# Identify the currently running release
+current_release=$(ls ../releases | sort -nr | head -n 1)
+now_in_unix_seconds=$(date +'%s')
+if [[ $current_release == '' ]]; then current_release=$now_in_unix_seconds; fi
+
+# Create release
+mix release --path ../releases/${now_in_unix_seconds}
