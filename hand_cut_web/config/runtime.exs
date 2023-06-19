640becd2836f64401d515eebf082aa0a81a3fecf
Mohamed Aly <mksaga@hey.com>
2023-06-19 14:09:27 -0400

    Fix path to runtime.exs

--- hand_cut_web/config/runtime.exs
+++ hand_cut_web/config/runtime.exs
@@ -8,10 +8,10 @@ import Config
 
 case config_env() do
   :dev ->
-    Code.require_file("runtime.exs", "../hand_cut/config")
+    Code.require_file("runtime.exs", "./hand_cut/config")
     Code.require_file("runtime.dev.exs", "config")
   :test -> Code.require_file("runtime.test.exs", "config")
   :prod ->
-    Code.require_file("runtime.exs", "../hand_cut/config")
+    Code.require_file("runtime.exs", "./hand_cut/config")
     Code.require_file("runtime.prod.exs", "config")
 end
