477ee708e252fed6a73bf5f17e964364752d09fc
Mohamed Aly <mksaga@hey.com>
2023-06-21 17:57:45 -0400

    Move script up

--- hand_cut_web/lib/hand_cut_web_web/live/results_live.ex
+++ hand_cut_web/lib/hand_cut_web_web/live/results_live.ex
@@ -74,6 +74,8 @@ defmodule HandCutWebWeb.ResultsLive do
 
   def render(assigns) do
     ~H"""
+    <script>(g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.src=`https://maps.${c}apis.com/maps/api/js?`+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})
+        ({key: "<%= @maps_key %>", v: "beta"});</script>
     <div class="container">
         <div class="level-item">
             <h1 class="title">Results</h1>
@@ -96,8 +98,6 @@ defmodule HandCutWebWeb.ResultsLive do
             </h2>
         </div>
     </div>
-    <script>(g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.src=`https://maps.${c}apis.com/maps/api/js?`+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})
-        ({key: "<%= @maps_key %>", v: "beta"});</script>
 
     <%= if length(@results) > 0 do %>
       <div class="container is-max-desktop mt-2">
