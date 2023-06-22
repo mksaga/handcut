78922da1dc83e24051dc6f626937d25d11755c52
Mohamed Aly <mksaga@hey.com>
2023-06-22 18:08:28 -0400

    Updates icon links

--- hand_cut_web/lib/hand_cut_web_web/live/results_live.ex
+++ hand_cut_web/lib/hand_cut_web_web/live/results_live.ex
@@ -77,22 +77,24 @@ defmodule HandCutWebWeb.ResultsLive do
     <script>(g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.src=`https://maps.${c}apis.com/maps/api/js?`+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})
         ({key: "<%= @maps_key %>", v: "beta"});</script>
     <div class="container">
+    <div class="level mb-1">
+    <button class="button">
+        <a phx-hook="BackHook" id="back" phx-update="ignore">
+            <div class="icon-text">
+            <span class="icon">
+                <ion-icon name="arrow-back-outline" class="ion-ionic arrow-back-outline"></ion-icon>
+            </span>
+            <span>Back</span>
+            </div>
+        </a>
+    </button>
+</div>
         <div class="level-item">
             <h2 class="title is-4">
               <%= length(@results) %> result<%= if length(@results) != 1 do "s" end %> in <%= Areas.humanize_area(@area) %>
             </h2>
             </div>
             <div class="level-item">
-            <h2>
-            <a phx-hook="BackHook" id="back" phx-update="ignore">
-              <div class="icon-text">
-                <span class="icon">
-                    <ion-icon name="arrow-back-outline" class="ion-ionic arrow-back-outline"></ion-icon>
-                </span>
-            <span>Back to search</span>
-              </div>
-            </a>
-            </h2>
         </div>
     </div>
 
