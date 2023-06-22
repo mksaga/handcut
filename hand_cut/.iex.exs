65eb9b976c1e5f6b2358545761adaf9648edf09b
Mohamed Aly <mksaga@hey.com>
2023-06-22 14:08:45 -0400

    Adds rest/cert id's to config

--- hand_cut/.iex.exs
+++ hand_cut/.iex.exs
@@ -1,2 +1,5 @@
 alias HandCut.Commands.{CreateRestaurant, UpdateRestaurant}
 alias HandCut.Runtime.App, as: HCApp
+
+restaurant_id = "restaurant_" <> Nanoid.generate(6, "0123456789abcdefghijklmnopqrstuvwxyz")
+cert_id = "certification_" <> Nanoid.generate(6, "0123456789abcdefghijklmnopqrstuvwxyz")
